37fb4b5ab8d8b66f12cfb689587395a3b706c7a7
henriquecmelo1 <hcm@cin.ufpe.br>
2024-10-01 00:50:12 -0300

    feat: appointment day and time on backend

--- backend/models/studentModel.py
+++ backend/models/studentModel.py
@@ -1,17 +1,29 @@
-from sqlalchemy import Column, Integer, String, Float, Date
+from sqlalchemy import  Integer, String, Float, Date, Time
 from db.base import Base
 from sqlalchemy.sql import func
+from sqlalchemy.types import Enum
+from sqlalchemy.orm import mapped_column, Mapped
+import enum
 
+class WeekDays(str, enum.Enum):
+        MONDAY = "Segunda-Feira"
+        TUESDAY = "Terça-Feira"
+        WEDNESDAY = "Quarta-Feira"
+        THURSDAY = "Quinta-Feira"
+        FRIDAY = "Sexta-Feira"
+        SATURDAY = "Sábado"
+        SUNDAY = "Domingo"
 
 class StudentModel(Base):
-    __tablename__ = "students"
-
-    cpf = Column(String(11), primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    birthday = Column(Date, nullable=False, default=func.current_date())
-    cellphone = Column(String(11), nullable=False)
-    payment_day = Column(Integer, default=1)
-    payment_value = Column(Float, default=0.0)
-    frequency = Column(Integer, default=1)
-    appointments = Column(String)
-    emergency_contact = Column(String)
+    __tablename__ = 'students'
+    
+    cpf: Mapped[str] = mapped_column(String(11), primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    birthday: Mapped[Date] = mapped_column(Date, nullable=False, default=func.current_date())
+    cellphone: Mapped[str] = mapped_column(String(11), nullable=False)
+    payment_day: Mapped[int] = mapped_column(Integer, default=1)
+    payment_value: Mapped[float] = mapped_column(Float, default=0.0)
+    frequency: Mapped[int] = mapped_column(Integer, default=1)
+    appointment_day: Mapped[WeekDays] = mapped_column(Enum(WeekDays), nullable=False)
+    appointment_time: Mapped[str] = mapped_column(Time, nullable=False)
+    emergency_contact: Mapped[str] = mapped_column(String)
--- backend/schemas/studentSchema.py
+++ backend/schemas/studentSchema.py
@@ -1,6 +1,9 @@
-from pydantic import BaseModel, Field
-from datetime import date
+from pydantic import BaseModel, Field, field_validator
+from datetime import date, time
 from typing import Optional
+
+from models.studentModel import WeekDays
+
 
 class StudentBase(BaseModel):
     cpf: str = Field(..., min_length=11, max_length=11, description="CPF deve ter 11 dígitos")
@@ -10,12 +13,14 @@ class StudentBase(BaseModel):
     payment_day: Optional[int] = Field(None, description="Dia do pagamento")
     payment_value: Optional[float] = Field(None, description="Valor do pagamento")
     frequency: Optional[int] = Field(None, description="Frequência de participação")
-    appointments: Optional[str] = Field(None, description="Compromissos")
+    appointment_day: Optional[WeekDays] = Field(None, description="Compromissos")
+    appointment_time: Optional[time] = Field(None, description="Horário do compromisso")
     emergency_contact: Optional[str] = Field(None, description="Contato de emergência")
+
 
 class StudentCreate(StudentBase):
     pass
 
 class Student(StudentBase):
     class Config:
-        orm_mode = True
+        from_attributes = True
