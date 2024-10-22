8c69e7d70133e18b19f1bf222e43758c7b331bdc
henriquecmelo1 <hcm@cin.ufpe.br>
2024-10-21 22:28:33 -0300

    feat: front-end post

--- frontend/src/app/components/forms/forms.component.ts
+++ frontend/src/app/components/forms/forms.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators, FormArray, FormControl} from '@angular/forms';
 import { StudentService } from '../../services/student.service';
 import { Router, RouterLink } from '@angular/router';
 
@@ -16,6 +16,7 @@ export class FormsComponent implements OnInit {
 
   userForm!: FormGroup;
 
+
   
   constructor(
     private fb: FormBuilder, 
@@ -26,6 +27,8 @@ export class FormsComponent implements OnInit {
 
   
   ngOnInit(): void {
+    
+
     this.userForm = this.fb.group({
       cpf: ['', [Validators.required, Validators.pattern('[0-9]{11}')]],
       name: ['', Validators.required],
@@ -34,9 +37,10 @@ export class FormsComponent implements OnInit {
       payment_day: ['', [Validators.min(1), Validators.max(31)]],
       payment_value: [''],
       frequency: ['',  Validators.min(1)],
-      appointments: [''],
+      appointment_day: this.fb.array([]),
+      appointment_time: this.fb.array([]),
       emergency_contact: ['']
-    });
+    });   
 
     // birthday default value
     const today = new Date().toISOString().split('T')[0];
@@ -49,14 +53,43 @@ export class FormsComponent implements OnInit {
     this.userForm.patchValue({ payment_value: 0 });
 
     // frequency default value
-    this.userForm.patchValue({ frequency: 1 });
+    this.userForm.patchValue({ frequency: 0 });
+
+    
 
     
   };
 
+  get appointmentDays(): FormArray {
+    return this.userForm.get('appointment_day') as FormArray;
+  }
+
+  get appointmentTimes(): FormArray {
+    return this.userForm.get('appointment_time') as FormArray;
+  }
+
+  onFrequencyChange(): void {
+    const frequency = this.userForm.get('frequency')?.value || 0;
+    this.adjustAppointmentArrays(frequency);
+  }
+
+  adjustAppointmentArrays(length: number): void {
+    while (this.appointmentDays.length !== length) {
+      if (this.appointmentDays.length < length) {
+        this.appointmentDays.push(this.fb.control(''));
+        this.appointmentTimes.push(this.fb.control(''));
+      } else {
+        this.appointmentDays.removeAt(this.appointmentDays.length - 1);
+        this.appointmentTimes.removeAt(this.appointmentTimes.length - 1);
+      }
+    }
+  }
+  
+
 
   onSubmit(form: any) {
     console.log(form.value)
+    
     if (form.valid) {
       this.studentService.createStudent(form.value).subscribe(response => {
         console.log('Student created successfully', response);
